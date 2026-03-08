90dbf35bd2d90469d9dbe08f3ad17ca1440cf9b6
prasetyasurya-ui <prasetya.surya@ui.ac.id>
2026-03-08 21:30:19 +0700

    [GREEN] Implement Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -8,14 +8,31 @@ import java.util.Map;
 @Setter
 @Getter
 public class Payment {
-    String id;
-    String method;
-    String status;
-    Map<String, String> paymentData;
+    private String id;
+    private String method;
+    private String status;
+    private Map<String, String> paymentData;
 
-    public Payment(String id, String method, Map<String, String > paymentData) {
+    public Payment(String id, String method, Map<String, String> paymentData) {
+        if (id == null || id.trim().isEmpty()) {
+            throw new IllegalArgumentException("Payment ID cannot be null or empty");
+        }
+        if (method == null || method.trim().isEmpty()) {
+            throw new IllegalArgumentException("Payment Method cannot be null or empty");
+        }
+        if (paymentData == null) {
+            throw new IllegalArgumentException("Payment Data cannot be null");
+        }
         this.id = id;
         this.method = method;
         this.paymentData = paymentData;
     }
+
+    public void setStatus(String status) {
+        if (!"SUCCESS".equals(status) && !"REJECTED".equals(status) && !"PENDING".equals(status)) {
+            throw new IllegalArgumentException("Invalid status");
+        }
+        this.status = status;
+    }
+
 }
